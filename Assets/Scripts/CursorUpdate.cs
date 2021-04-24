cbe4a3a25e36890b8357419e7732fd7d9195d9e5
Max <madmanmax120@hotmail.com>
2021-04-24 23:59:47 +1000

    Added Cursor trail effect

--- Assets/Scripts/CursorUpdate.cs
+++ Assets/Scripts/CursorUpdate.cs
@@ -4,17 +4,41 @@ using UnityEngine;
 
 public class CursorUpdate : MonoBehaviour
 {
-    public float speed = 0.05f;
-    public Vector2 offset;
+    public float speed = 0.1f;
+    private Vector2 mousePos;
+
+    public Transform[] shadows;
 
     // Start is called before the first frame update
     private IEnumerator Start()
     {
         while (true)
-        { 
-            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        {
+            shadows[1].position = shadows[0].position;
+            shadows[0].position = mousePos;
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             transform.position = mousePos;
+            Debug.Log("Main cursor: " + transform.position +
+                "\nShadow 1: " + shadows[0].position +
+                "\nShadow 2: " + shadows[1].position);
+
             yield return new WaitForSeconds(speed);
+
+            /// Weird trail effect
+            //mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            //transform.position = mousePos;
+
+            //yield return new WaitForSeconds(speed / 3);
+            //shadows[0].position = mousePos;
+
+
+            //yield return new WaitForSeconds(speed / 3);
+            //shadows[1].position = shadows[0].position;
+
+            //yield return new WaitForSeconds(speed / 3);
+            //Debug.Log("Main cursor: " + transform.position +
+            //    "\nShadow 1: " + shadows[0].position +
+            //    "\nShadow 2: " + shadows[1].position);
         }
     }
 }
