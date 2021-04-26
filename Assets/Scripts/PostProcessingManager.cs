bdccd3ac68f4c30ac6a2a897bd163a8b18f74162
Max <madmanmax120@hotmail.com>
2021-04-26 22:18:12 +1000

    Added DEEPER SPIRAL INTO HELL

--- Assets/Scripts/PostProcessingManager.cs
+++ Assets/Scripts/PostProcessingManager.cs
@@ -7,21 +7,40 @@ public class PostProcessingManager : MonoBehaviour
 {
     public static PostProcessingManager Inst;
 
-    public PostProcessVolume PPV;
+    public PostProcessVolume PPVNormal;
+    public PostProcessVolume PPVDEEP;
+    public PostProcessVolume PPVDEEPER;
     private bool IsPostProcessing = true;
     public int IntensityVolume = 20;
+
+    private bool DEEPER = false;
+    private float DEEPtarget = 0;
+    private float DEEPmultiplier = 0.25f; // Higher makes the transition happen faster
 
     // Start is called before the first frame update
     void Start()
     {
+        GameManager.ConsecutiveDeepVideosUpdated += IncrementDEEP;
         InitialSetup();
+    }
+
+    private void Update()
+    {
+        PPVDEEP.weight = Mathf.Lerp(PPVDEEP.weight, DEEPtarget, 5 * Time.deltaTime);
+
+        if (DEEPER)
+        {
+            PPVDEEPER.weight += 0.075f * Time.deltaTime;
+        }
     }
 
     private void InitialSetup()
     {
         SetSingleton();
-        if (PPV == null)
-            PPV = gameObject.GetComponent<PostProcessVolume>();
+        if (PPVNormal == null)
+        {
+            PPVNormal = gameObject.GetComponentInChildren<PostProcessVolume>();
+        }
     }
 
     private void SetSingleton()
@@ -35,12 +54,23 @@ public class PostProcessingManager : MonoBehaviour
     public void TogglePostPrcessing(bool _Toggle)
     {
         LensDistortion LD;
-        PPV.TryGetComponent<LensDistortion>(out LD);
+        PPVNormal.TryGetComponent<LensDistortion>(out LD);
         if (_Toggle)
             LD.intensity.value = IntensityVolume;
         else
             LD.intensity.value = 0;
 
         IsPostProcessing = _Toggle;
+    }
+
+    public void IncrementDEEP(int i)
+    {
+        DEEPtarget = i * DEEPmultiplier;
+        //PPVDEEP.weight += DEEPtarget;
+        Debug.Log("DEEP target: " + DEEPtarget);
+        if (PPVDEEP.weight >= 1)
+        {
+            DEEPER = true;
+        }
     }
 }
